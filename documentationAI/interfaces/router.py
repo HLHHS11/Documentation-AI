46663c39c16d2121db46cb6dd77b8597f4ca0d93
HLHHS11 <chiryucity@gmail.com>
2023-08-07 00:19:11 +0900

    add: CLIで使用するコマンドのルータークラスを定義

---
+++ documentationAI/interfaces/router.py
@@ -0,0 +1,21 @@
+from typing import Callable, Dict
+
+# from documentationAI.interfaces.handlers import Handlers
+
+
+class Router:
+    def __init__(self, routes: Dict[str, Callable[[list[str]], None]]):
+        # self.routes: Dict[str, Callable[[list[str]], None]] = {}
+        self.routes = routes
+
+    def register(self, request: str, callback: Callable[[list[str]], None]) -> None:
+        self.routes[request] = callback
+    
+    def handle(self, request: str, params: list[str]) -> None:
+        if request in self.routes:
+            handler = self.routes[request]
+            handler(params)
+        else:
+            print("Invalid command. Type 'help' for available commands.")
+
+
