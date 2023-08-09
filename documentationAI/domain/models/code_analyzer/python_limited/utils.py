f771b1d2fc204f3f8a5fcd9c0ffe1b40335619ff
HLHHS11 <chiryucity@gmail.com>
2023-08-10 03:18:13 +0900

    add: パスとネームスペースの相互変換を行う関数を追加

---
+++ documentationAI/domain/models/code_analyzer/python_limited/utils.py
@@ -0,0 +1,18 @@
+# FIXME: プロジェクトルートディレクトリ名とパッケージ名が同じ場合，正しいネームスペースが得られない！！！
+import os
+
+def filepath_to_namespace(filepath: str, package_name: str = "") -> str:
+    # ファイルパスの/や\を.に置換し、.pyを削除する
+    namespace = filepath.replace(os.sep, '.').replace('.py', '')
+
+    # package_nameが指定されていた場合、ネームスペースをそのパッケージ名で始まるものに変換する
+    if package_name:
+        package_name_with_dot = package_name + '.'
+        if package_name_with_dot in namespace:
+            namespace = namespace[namespace.index(package_name_with_dot):]
+
+    return namespace
+
+# HACK: 絶対パスも得られるようになると便利そうなんだが…
+def namespace_to_relativepath(namespace: str) -> str:
+    return namespace.replace('.', os.sep) + '.py'
