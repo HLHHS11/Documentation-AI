082b89d9bd2d039221054db965f2cc020b0fbe9b
HLHHS11 <chiryucity@gmail.com>
2023-08-10 02:58:33 +0900

    add. テストを追加

---
+++ documentationAI/test/domain/models/code_analyzer/python_limited/code_analyzer.py
@@ -0,0 +1,12 @@
+from documentationAI.domain.models.code_analyzer.python_limited.dependencies_analyzer import PythonDependenciesAnalyzer
+from documentationAI.domain.models.code_analyzer.python_limited.code_analyzer import PythonCodeAnalyzer
+
+def test_python_code_analyzer():
+    # NOTE: 以下のfilepathは，テスト実行環境に応じて変更する必要があります。本来はコード自体を環境に依存しないようにする必要がありますが…
+    package_name = "documentationAI"
+    package_root_dir = "/home/yama/Documents/Programming/documentation-AI/documentationAI"
+    code_analyzer = PythonCodeAnalyzer(PythonDependenciesAnalyzer(package_name))
+    dag = code_analyzer.generate_dag(package_root_dir)
+    return dag
+
+print(test_python_code_analyzer())
