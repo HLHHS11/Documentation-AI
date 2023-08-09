3ad06811a17165ed820d083b87a932554b50ad0a
HLHHS11 <chiryucity@gmail.com>
2023-08-10 02:12:25 +0900

    update: 種々の抽象クラスを追加

--- documentationAI/domain/models/code_analyzer/abc.py
+++ documentationAI/domain/models/code_analyzer/abc.py
@@ -1,6 +1,29 @@
 import abc
+from typing import Dict, Tuple
 
-class CodeAnalyzer(abc.ABC):
+class ISymbolInfo(abc.ABC):
+
     @abc.abstractmethod
-    def analyze(self, code: str) -> None:
+    def stringify(self) -> str:
         pass
+
+
+class IDependenciesAnalyzer(abc.ABC):
+
+    @abc.abstractmethod
+    def analyze(self, file_path: str) -> Tuple[str, Dict[str, list[ISymbolInfo]]]:
+        pass
+
+
+class ICodeAnalyzer(abc.ABC):
+
+    def __init__(
+        self, dependencies_analyzer: IDependenciesAnalyzer
+    ):
+        self.dependencies_analyzer = dependencies_analyzer
+
+    @abc.abstractmethod
+    def generate_dag(self, root_dir: str) -> Dict[str, list[str]]:
+        pass
+
+
