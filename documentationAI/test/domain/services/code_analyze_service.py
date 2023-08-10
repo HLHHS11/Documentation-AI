99b4f203ff2b9189f69cdfd60c7741be3f1429d6
HLHHS11 <chiryucity@gmail.com>
2023-08-11 05:31:39 +0900

    fix: ちゃんとコード解析サービスのテストを書いた

--- documentationAI/test/domain/services/code_analyze_service.py
+++ documentationAI/test/domain/services/code_analyze_service.py
@@ -1,18 +1,13 @@
-# NOTE: ここに書くべきテストではないかも！？しかも他のテストコードに依存してるし…
-# dagを取得してトポロジカルソートするサンプルコード
+from documentationAI.container import container
+from documentationAI.domain.services.code_analyze_service import CodeAnalyzeService
 
-from documentationAI.utils.topological_sort import topological_sort
-from documentationAI.domain.models.code_analyzer.python_limited.code_analyzer import PythonCodeAnalyzer
-from documentationAI.domain.models.code_analyzer.python_limited.dependencies_analyzer import PythonDependenciesAnalyzer
 
-def test_analysis_sort():
-    # NOTE: 以下のfilepathは，テスト実行環境に応じて変更する必要があります。本来はコード自体を環境に依存しないようにする必要がありますが…
-    package_name = "documentationAI"
+def code_analyze_service_test():
+    container.package_name.override("documentationAI")  # type: ignore
+    service: CodeAnalyzeService = container.code_analyze_service()
     package_root_dir = "/home/yama/Documents/Programming/documentation-AI/documentationAI"
-    code_analyzer = PythonCodeAnalyzer(PythonDependenciesAnalyzer(package_name))
-    dag = code_analyzer.generate_dag(package_root_dir)
-    return topological_sort(dag)
+    # package_name = "documentationAI"
+    return service.resolve_dependencies(package_root_dir)
 
-result = test_analysis_sort()
-for symbol_info in result:
-    print(symbol_info)
+
+print(code_analyze_service_test())
