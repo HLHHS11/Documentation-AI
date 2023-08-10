49fbc6c1be7aabc13961d9724efe3187f1fef656
HLHHS11 <chiryucity@gmail.com>
2023-08-10 17:26:14 +0900

    fix: さすがにテストコードが他に依存しているのはマズいので修正した

--- documentationAI/test/domain/services/code_analyze.py
+++ documentationAI/test/domain/services/code_analyze.py
@@ -1,11 +1,16 @@
 # NOTE: ここに書くべきテストではないかも！？しかも他のテストコードに依存してるし…
 # dagを取得してトポロジカルソートするサンプルコード
 
-from documentationAI.test.domain.models.code_analyzer.python_limited.code_analyzer import test_python_code_analyzer
 from documentationAI.utils.topological_sort import topological_sort
+from documentationAI.domain.models.code_analyzer.python_limited.code_analyzer import PythonCodeAnalyzer
+from documentationAI.domain.models.code_analyzer.python_limited.dependencies_analyzer import PythonDependenciesAnalyzer
 
 def test_analysis_sort():
-    dag = test_python_code_analyzer()
+    # NOTE: 以下のfilepathは，テスト実行環境に応じて変更する必要があります。本来はコード自体を環境に依存しないようにする必要がありますが…
+    package_name = "documentationAI"
+    package_root_dir = "/home/yama/Documents/Programming/documentation-AI/documentationAI"
+    code_analyzer = PythonCodeAnalyzer(PythonDependenciesAnalyzer(package_name))
+    dag = code_analyzer.generate_dag(package_root_dir)
     return topological_sort(dag)
 
 result = test_analysis_sort()
