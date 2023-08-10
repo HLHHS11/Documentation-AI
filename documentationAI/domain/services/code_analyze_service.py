8e732370a10f350724d2ba0de58afe11d72f1c2c
HLHHS11 <chiryucity@gmail.com>
2023-08-11 03:40:17 +0900

    add: サービスクラスの説明をコメントで追加

--- documentationAI/domain/services/code_analyze_service.py
+++ documentationAI/domain/services/code_analyze_service.py
@@ -2,6 +2,8 @@ from documentationAI.domain.models.code_analyzer.abc import ICodeAnalyzer
 from documentationAI.utils.topological_sort import topological_sort
 
 
+# NOTE: `ICodeAnalyzer`が提供する機能に対して目新しい機能はないが，こちらは実装クラスである点がメリットと考えられる。
+#       すなわち，抽象クラスを扱う役割をこのクラスが引き受けることで，外側のレイヤからは抽象クラスを意識する必要がなくなる。（常に実装クラスを呼び出せば良い）
 class CodeAnalyzeService:
 
     def __init__(self, code_analyzer: ICodeAnalyzer) -> None:
