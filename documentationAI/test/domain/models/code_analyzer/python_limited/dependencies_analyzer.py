72134d297d1787db5502479e34a0b50f4449a81e
HLHHS11 <chiryucity@gmail.com>
2023-08-11 10:02:10 +0900

    update: 定義文取得機能のテストを追加

--- documentationAI/test/domain/models/code_analyzer/python_limited/dependencies_analyzer.py
+++ documentationAI/test/domain/models/code_analyzer/python_limited/dependencies_analyzer.py
@@ -5,7 +5,7 @@ import documentationAI.domain.models.code_analyzer.python_limited.dependencies_a
 
 def test_analyze_dependencies():
     # NOTE: 以下のfilepathは，テスト実行環境に応じて変更する必要があります。本来はコード自体を環境に依存しないようにする必要がありますが…
-    file_path = "/home/yama/Documents/Programming/documentation-AI/documentationAI/foobar.py"
+    file_path = "/home/yama/Documents/Programming/documentation-AI/documentationAI/domain/models/code_analyzer/python_limited/dependencies_analyzer.py"
     package_name = "documentationAI"
     parser = dependencies_analyzer.parse_python_symbol_str
     analyzer = dependencies_analyzer.PythonDependenciesAnalyzer(package_name, parser)
@@ -15,5 +15,17 @@ def test_analyze_dependencies():
         print(symbol_name)
         for dependency in dependencies:
             print(f"\t{dependency.namespace}.{dependency.symbol_name}")
-
+    
 test_analyze_dependencies()
+
+print("=========")
+
+def test_get_symbol_definition():
+    file_path = "/home/yama/Documents/Programming/documentation-AI/documentationAI/domain/models/code_analyzer/python_limited/dependencies_analyzer.py"
+    package_name = "documentationAI"
+    parser = dependencies_analyzer.parse_python_symbol_str
+    analyzer = dependencies_analyzer.PythonDependenciesAnalyzer(package_name, parser)
+    symbol_definition = analyzer.get_symbol_definition(file_path, "PythonDependenciesAnalyzer.analyze")
+    return symbol_definition
+
+print(test_get_symbol_definition())
