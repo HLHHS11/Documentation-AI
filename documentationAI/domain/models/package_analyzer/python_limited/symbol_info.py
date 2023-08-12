f2fc1af27db78c1c04b188a670550f5fcc507f29
HLHHS11 <chiryucity@gmail.com>
2023-08-13 03:34:19 +0900

    add: シンボル情報クラスを別ファイルに切り出した

---
+++ documentationAI/domain/models/package_analyzer/python_limited/symbol_info.py
@@ -0,0 +1,26 @@
+from documentationAI.domain.models.package_analyzer.abc import ISymbolInfo
+
+
+class PythonSymbolInfo(ISymbolInfo):
+
+    def __init__(self, namespace: str, symbol_name: str):
+        self.namespace: str = namespace
+        self.symbol_name: str = symbol_name
+
+    # ネームスペースとシンボル名を:で結合した文字列を返す 
+    def stringify(self) -> str:
+        return f"{self.namespace}:{self.symbol_name}"
+    
+    @classmethod
+    def parse(cls, stringified: str) -> 'PythonSymbolInfo':
+        namespace, symbol_name = stringified.split(':')
+        return cls(namespace, symbol_name)
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, PythonSymbolInfo):
+            return self.namespace == other.namespace and self.symbol_name == other.symbol_name
+        else:
+            return False
+    
+    def __str__(self) -> str:
+        return f"PythonSymbolInfo(namespace={self.namespace}, symbol_name={self.symbol_name})"
