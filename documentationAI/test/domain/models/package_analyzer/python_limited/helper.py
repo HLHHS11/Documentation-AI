b67c692da33ea83cb2f11342005f18274c71c559
HLHHS11 <chiryucity@gmail.com>
2023-08-16 02:45:00 +0900

    update: 変更に対応

--- documentationAI/test/domain/models/package_analyzer/python_limited/helper.py
+++ documentationAI/test/domain/models/package_analyzer/python_limited/helper.py
@@ -6,15 +6,22 @@ def test_parse_symbol_str():
 
 def test_abspath_to_namespace():
     helper = PythonAnalyzerHelper()
-    assert helper.abspath_to_namespace(
-        "/home/yama/Documents/Programming/documentation-AI/documentationAI/domain/models/package_analyzer/python_limited/package_analyzer.py", "documentationAI"
-    ) == "documentationAI.domain.models.package_analyzer.python_limited.package_analyzer"
+    print(
+        helper.abspath_to_namespace(
+            "/home/yama/Documents/Programming/documentation-AI/documentationAI/domain/models/package_analyzer/python_limited/package_analyzer.py",
+            "/home/yama/Documents/Programming/documentation-AI/"
+        ) == "documentationAI.domain.models.package_analyzer.python_limited.package_analyzer"
+    )
 
 def test_namespace_to_abspath():
     helper = PythonAnalyzerHelper()
-    assert helper.namespace_to_abspath(
-        "documentationAI.domain.models.package_analyzer.python_limited.package_analyzer", "/home/yama/Documents/Programming/documentation-AI/documentationAI"
-    ) == "/home/yama/Documents/Programming/documentation-AI/documentationAI/domain/models/package_analyzer/python_limited/package_analyzer.py"
+    print(
+        helper.namespace_to_abspath(
+            "documentationAI.domain.models.package_analyzer.python_limited.package_analyzer",
+            "/home/yama/Documents/Programming/documentation-AI"
+        ) == "/home/yama/Documents/Programming/documentation-AI/documentationAI/domain/models/package_analyzer/python_limited/package_analyzer.py"
+    )
 
 
 test_abspath_to_namespace()
+test_namespace_to_abspath()
