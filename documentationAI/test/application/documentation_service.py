c53a12ee9ebe320d4000126570943049b946e1fc
HLHHS11 <chiryucity@gmail.com>
2023-09-09 22:29:22 +0900

    fix: 仕様変更に対応

--- documentationAI/test/application/documentation_service.py
+++ documentationAI/test/application/documentation_service.py
@@ -6,6 +6,7 @@ def documentation_generator_service_test():
     container.package_name.override("documentationAI")  # type: ignore
     application_service: DocumentationService = container.documentation_service()
     root_dir = "/home/yama/Documents/Programming/documentation-AI"
+    package_root_dir = "/home/yama/Documents/Programming/documentation-AI/documentationAI"
     package_name = "documentationAI"
     application_service.generate_package_documentation(root_dir, package_root_dir, package_name)
 
